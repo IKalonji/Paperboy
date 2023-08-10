6010fb53c2d57bc18bd5c30eb7198639e2ac98f2
IKalonji <IKalonji@student.wethinkcode.co.za>
2023-08-10 11:24:10 +0200

    Added setup method to fix failing test

--- backend-python/tests.py
+++ backend-python/tests.py
@@ -5,31 +5,34 @@ from generators.evm_generator import EvmAddrGenerator
 
 class TestGenerators(unittest.TestCase):
 
+    def setUp(self):
+        """Create uuid_files Folder"""
+        try:
+            os.mkdir("uuid_files")
+        except FileExistsError as error:
+            print("Skipping, folder exists")
+
     def test_btc_generator(self):
+        """Test BTC Generator"""
         generator = BtcAddrGenerator()
         result = generator.generate()
         self.assertEqual(result.get("result"), "ok", "Should be ok")
         self.assertEqual(result.get("error"), "", "Should not have an error")
 
     def test_evm_generator(self):
+        """Test EVM Generator"""
         generator = EvmAddrGenerator()
         result = generator.generate()
         self.assertEqual(result.get("result"), "ok", "Should be ok")
         self.assertEqual(result.get("error"), "", "Should not have an error")
 
     def test_writer(self):
+        """Test Writer"""
         generator = EvmAddrGenerator()
-        try:
-            os.mkdir("uuid_files")
-        except FileExistsError as error:
-            print("Skipping, folder exists")
         result = generator.writer("hello", "hello_from_test")
         self.assertEqual(result, True, "Should be return true")
         self.assertEqual(os.path.isfile(os.path.join(os.getcwd(),"uuid_files", "hello_from_test.txt")), True, "File should be created")
-        try:
-            os.rmdir("uuid_files")
-        except Exception as error:
-            print("Could not delete folder")
+        
 
 if __name__ == '__main__':
     unittest.main()
